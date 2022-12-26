dbd95003074b1f63a8a38c7ffe6ea312be4ad981
John Rey <adioskid@mail.ru>
2022-12-26 21:36:07 +0300

    racing

--- src/Start.java
+++ src/Start.java
@@ -1,4 +1,7 @@
+import transport.*;
 public class Start {
+    public static void main(String[] args) {
 
+    }
 }
 
--- src/transport/Transport.java
+++ src/transport/Transport.java
@@ -1,6 +1,38 @@
 package transport;
 
-class Transport  {
+import lombok.Getter;
+
+@Getter
+abstract class Transport {
+    private final String model;
+    private final String brand;
+    private double engineCapacity;
+
+    Transport(String model, String brand, double engineCapacity) {
+        String def = "default";
+        if (model == null || model.isBlank()) {
+            System.out.println("Введите модель автомобиля.");
+            model = def;
+        }
+        this.model = model;
+        if (brand == null || brand.isBlank()) {
+            System.out.println("Введите марку автомобиля.");
+            brand = def;
+        }
+        this.brand = brand;
+        setEngineCapacity(engineCapacity);
+    }
+
+    void setEngineCapacity(double engineCapacity) {
+        if (engineCapacity <= 0) {
+            engineCapacity = 1.0;
+            System.out.println("Внимание! Малый литраж автомобиля!");
+        }
+        this.engineCapacity = engineCapacity;
+    }
+
+    abstract String startDriving();
+    abstract String stopDriving();
 }
 
 
